496a87fbdc77d7c86ac3952a322968e7d7de180d
Bryan Pereyra <bryan.pereyra@unosquare.com>
2023-03-27 12:59:46 -0600

    Data table

--- cypress/e2e/features/Amazon.feature
+++ cypress/e2e/features/Amazon.feature
@@ -1,6 +1,12 @@
 Feature: Search for an item and print its price in console
 
-  Scenario: T1: Search for Iphone 14 Pro Max
-    Given Go to www.amazon.com
-    When Search for Iphone 14 Pro Max
-    Then Click on the first result and enter item details page
+  Scenario Outline: Search for '<itemName>'
+    Given I navigate to the website
+    When Search for an '<itemName>' and click on the first result
+    Then Store the price and print it
+
+    Examples:
+      | itemName           |
+      | iPhone 14 Pro Max  |
+      | Macbook Air        |
+      | Samsung Galaxy S23 |
--- cypress/e2e/step_definitions/amazon.js
+++ cypress/e2e/step_definitions/amazon.js
@@ -1,18 +1,18 @@
-import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor'
+import { Given, When, And, Then } from '@badeball/cypress-cucumber-preprocessor'
 import { onDetailsPage } from "../../support/page_objects/pages/detailsPage"
 import { onHomePage } from "../../support/page_objects/pages/homePage"
 import { onResultsPage } from "../../support/page_objects/pages/resultsPage"
 const items = require("../../support/page_objects/testData/items.json");
 
-Given('Go to www.amazon.com', () => {
+Given('I navigate to the website', () => {
     cy.openHomePage()
 });
 
-When('Search for Iphone 14 Pro Max', () => {
-    onHomePage.searchFor(items.iPhone14ProMax)
+When('Search for an {string} and click on the first result', (itemName) => {
+    onHomePage.searchFor(itemName)
+    onResultsPage.selectFirstResult()
 });
 
-Then('Click on the first result and enter item details page', () => {
-    onResultsPage.selectFirstResult()
+Then('Store the price and print it', () => {
     onDetailsPage.storeAndPrintPrice()
 });
